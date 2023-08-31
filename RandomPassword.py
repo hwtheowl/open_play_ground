96156235340d30398700f486c3b287cf5e3bc879
hwtheowl <jegal7000@gmail.com>
2023-08-31 15:38:28 +0900

    update RandomPassword

--- RandomPassword.py
+++ RandomPassword.py
@@ -1,20 +1,35 @@
-# 조건1 대문자, 소문자, 특수문자(!@#$%^로 제한), 숫자 각각 1자 이상
-# 조건2 총 10자
+# 조건1 대문자, 소문자, 특수문자(!@#$로 제한), 숫자 각각 1자 이상
+# 조건2 비밀번호 자릿수 기본값 10(조절 가능)
 # 조건3 같은 문자 3회 연속 x
+# 조건4 txt 파일 만들어서 비밀번호 저장
 
-import random
-import string
+import random, string
+from datetime import date
 
-def generate_password():
-    limited_special_chars = "!@#$%^"
+def generate_password(symbol=None, save=True, path="./password.txt", k=10):
+    # 기본 특수문자
+    if symbol is None:
+        symbol = "!@#$"
+
     while True:
-        password = ''.join(random.choices(string.ascii_letters + string.digits + limited_special_chars, k=10))
+        # create password
+        password = ''.join(random.choices(string.ascii_letters + string.digits + symbol, k=k))
+
+        # check password
         if (any(c.islower() for c in password) and
             any(c.isupper() for c in password) and
             any(c.isdigit() for c in password) and
-            any(c in limited_special_chars for c in password) and
+            any(c in symbol for c in password) and
             not any(c * 3 in password for c in set(password))):
-            return password
+            break
+        
+    # 저장여부 확인 및 저장
+    if save:
+        with open(path, "a") as f:
+            data = str(date.today()) + ": " + password
+            f.write(data + "\n")
+        
+    return password
 
-password = generate_password()
+password = generate_password(k=16)
 print(password)
